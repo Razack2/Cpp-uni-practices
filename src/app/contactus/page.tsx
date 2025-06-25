6debaafeae08e561a71f0e967c59dc6241e131a1
Astro-Chimwala <astro4chimwala@gmail.com>
2025-06-25 17:18:57 +0200

    page

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = ()=>{
     return (
-        <div><h1><h1>This part is under development</h1></div>
+        <div><h1>This part is under development</h1></div>
     );
 
 }
